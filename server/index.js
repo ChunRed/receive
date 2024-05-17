e14afaf1efee9970c6f6093eae6b6d41cc537e06
sonicstride <sonicstride.tw@gmail.com>
2024-05-17 22:58:37 +0800

    add app.get

--- server/index.js
+++ server/index.js
@@ -2,11 +2,19 @@ const express = require("express");
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const engine = require('ejs-locals');
+
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
 
 
+app.set('views', "./views");
 
-app.use("/", (reqq, res) => {
-    res.send("server is running.");
+app.get('/', function (req, res) {
+    res.send("server is running.")
+});
+app.get('/demo', function (req, res) {
+    res.render('demo');
 });
 
 io.on('connection', function (socket) {
