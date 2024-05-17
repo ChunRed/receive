9739268f9a5f93770e644cd408faad98237012bf
sonicstride <sonicstride.tw@gmail.com>
2024-05-17 22:16:02 +0800

    add socketio

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
 
 
 app.use("/", (reqq, res) => {
     res.send("server is running.");
+});
+
+io.on('connection', function (socket) {
+    console.log('a user connected, user id: ' + socket.userId);
 });
 
 app.listen(3000, console.log("port: 3000"));
