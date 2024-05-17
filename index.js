2bae446414c1038ce62cf0945962d53c1d3e5feb
sonicstride <sonicstride.tw@gmail.com>
2024-05-17 16:10:29 +0800

    init

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var engine = require('ejs-locals');
+
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.static('node_modules'));
+app.set('views', "./views");
+
+app.get('/demo', function (req, res) {
+    res.render('demo');
+});
+
+
+
+http.listen(3000, function () {
+    console.log('listening on *:3000');
+});
